9d8b41a9a4ab333caf28aec5bed9c6fcd5f1809b
Roisin-M <roisinmuldoon00@gmail.com>
2023-01-25 10:28:35 +0000

    part a and b

--- Worksheet1/Program.cs
+++ Worksheet1/Program.cs
@@ -1,10 +1,38 @@
 ﻿namespace Worksheet1
 {
     internal class Program
-    {
+
+    { 
+        // declare array to store all integers between 40 and 60
+        static int[] integers = { 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 52, 52, 53, 54, 55, 56, 57, 58, 59 };
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            PrintIntegersWhile();
+        }
+        // method for a for loop
+        static private void PrintIntegersFor()
+        { 
+            
+            for(int i=0; i< integers.Length; i++)
+            {
+                Console.WriteLine(integers[i]); 
+            }
+        }
+        // method for a while loop
+        static private void PrintIntegersWhile()
+        {
+            
+            int i = 0;
+            while(i<integers.Length)
+            {
+                Console.WriteLine(integers[i]);
+                i++;
+            }
+        }
+        // method for a do while loop
+        static private void PrintIntegersDo()
+        {
+
         }
     }
 }
