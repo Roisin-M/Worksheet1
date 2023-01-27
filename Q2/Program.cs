cad000fc13e295eb24b2076a6ec6e661aacc578f
Roisin-M <roisinmuldoon00@gmail.com>
2023-01-27 10:38:40 +0000

    part a of question 2 is complete

--- Q2/Program.cs
+++ Q2/Program.cs
@@ -8,6 +8,7 @@
             Console.WriteLine($"Number of odd numbers is {(CountOddNumbers())}");
             
         }
+        //Part A
         static int CountOddNumbers()
         {
             int[] numberinput = new int[5];
@@ -24,28 +25,5 @@
             return countOfOdds;
         }
 
-            /*for (int i = 0; i < numberOdd.Length; i++)
-            {
-                Console.WriteLine($"{(numberOdd[i]),-30}");
-            }
-            */
-            /*int i = 0;
-            int[]oddNumber=new int[5];
-            do
-            {
-                Console.Write("enter a number value: ");
-                numberinput[i] = int.Parse(Console.ReadLine());
-                i++;
-                if (i % 2 != 0)
-                {
-                    numberinput[i]=oddNumber[i];
-                }
-            }
-            while(i < numberinput.Length);*/
-            /*for (int c = 0; c < oddNumber.Length; i++)
-            {
-                Console.WriteLine($"{(oddNumber[c]),-30}");
-            }*/
-        
     }
 }
