193f536ec90ab95f66abfb91089e7b0fccd38427
Roisin-M <roisinmuldoon00@gmail.com>
2023-01-27 10:37:34 +0000

    Completed q2

--- Q2/Program.cs
+++ Q2/Program.cs
@@ -4,22 +4,48 @@
     {
         static void Main(string[] args)
         {
-            CountOddNumbers();
+
+            Console.WriteLine($"Number of odd numbers is {(CountOddNumbers())}");
             
         }
-        static void CountOddNumbers()
+        static int CountOddNumbers()
         {
             int[] numberinput = new int[5];
-
-            for (int i = 0; i < numberinput.Length; i++)
+            int countOfOdds = 0;
+            for (int i = 0; i < 5; i++)
             {
                 Console.Write("enter a number value: ");
                 numberinput[i] = int.Parse(Console.ReadLine());
-                if(i%2 !=0)
+                if (numberinput[i] % 2 != 0)
                 {
-                    Console.WriteLine(i);
+                    countOfOdds++;
                 }
             }
+            return countOfOdds;
         }
+
+            /*for (int i = 0; i < numberOdd.Length; i++)
+            {
+                Console.WriteLine($"{(numberOdd[i]),-30}");
+            }
+            */
+            /*int i = 0;
+            int[]oddNumber=new int[5];
+            do
+            {
+                Console.Write("enter a number value: ");
+                numberinput[i] = int.Parse(Console.ReadLine());
+                i++;
+                if (i % 2 != 0)
+                {
+                    numberinput[i]=oddNumber[i];
+                }
+            }
+            while(i < numberinput.Length);*/
+            /*for (int c = 0; c < oddNumber.Length; i++)
+            {
+                Console.WriteLine($"{(oddNumber[c]),-30}");
+            }*/
+        
     }
 }
