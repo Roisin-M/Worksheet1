efe8cd3ec1afb28b111bf7b2c816960236b1f0d8
Roisin-M <roisinmuldoon00@gmail.com>
2023-01-27 09:35:28 +0000

    Completed q1

--- Worksheet1/Program.cs
+++ Worksheet1/Program.cs
@@ -5,12 +5,18 @@
     { 
         // declare array to store all integers between 40 and 60
         static int[] integers = { 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 52, 52, 53, 54, 55, 56, 57, 58, 59 };
+        static int number1 = 40;
+        static int number2 = 60;
+        static int number3 = 59;
+        static int excludenumber1 = 46;
+        static int excludenumber2 = 48;
+
         static void Main(string[] args)
         {
-            PrintExcludingFor();
+            PrintDescendingOrder();
         }
-        // method for a for loop
-        static private void PrintIntegersFor()
+        // method for a for loop A
+        static private void PrintIntegeresFor()
         { 
             
             for(int i=0; i< integers.Length; i++)
@@ -18,7 +24,7 @@
                 Console.WriteLine(integers[i]); 
             }
         }
-        // method for a while loop
+        // method for a while loop B
         static private void PrintIntegersWhile()
         {
             
@@ -29,7 +35,7 @@
                 i++;
             }
         }
-        // method for a do while loop
+        // method for a do while loop C
         static private void PrintIntegersDo()
         {
             int i = 0;
@@ -41,18 +47,26 @@
             while(i<integers.Length);
 
         }
-        // method for a for loop excluding 46 and 48
+        // method for a for loop excluding 46 and 48 D
         static private void PrintExcludingFor()
         {
-            for(int i=0; i<integers.Length;i++)
+            
+            for(int i=number1; i<number2; i++)
             {
-                if(i != 46 && i != 48)
+                if(i != excludenumber1 && i != excludenumber2)
                 {
-                    Console.WriteLine(integers[i]);
-                    
+                    Console.WriteLine(i);       
                 }
             }
             
+        }
+        //method for a for loop that prints in descending order E
+        static private void PrintDescendingOrder()
+        {
+            for(int i=number3; i>number1;i--)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
