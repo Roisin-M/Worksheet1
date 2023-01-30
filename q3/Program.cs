a53eb345da904af9c1e0b4aec8a3b4c1cf4189c0
roisin-M <roisinmuldoon00@gmail.com>
2023-01-30 09:30:23 +0000

    part a of q3 complete

---
+++ q3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintTriangle();
+        }
+        //A method to print one "#" at a time (a)
+        static private void PrintTriangle()
+        {
+            string symbol = "#";
+            string symbol2 = "#";
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(symbol);
+                symbol = symbol + symbol2;
+            }
+        }
+
+    }
+}
