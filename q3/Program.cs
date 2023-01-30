ec961ec0377486f69a612ae75fec9eee44ca2546
roisin-M <roisinmuldoon00@gmail.com>
2023-01-30 10:44:46 +0000

    q3 part a,b,c

--- q3/Program.cs
+++ q3/Program.cs
@@ -4,19 +4,73 @@
     {
         static void Main(string[] args)
         {
-            PrintTriangle();
+            PrintTriangleGeneral();
         }
         //A method to print one "#" at a time (a)
         static private void PrintTriangle()
         {
-            string symbol = "#";
+            /*string symbol = "#";
             string symbol2 = "#";
             for (int i = 0; i < 5; i++)
             {
                 Console.WriteLine(symbol);
                 symbol = symbol + symbol2;
             }
+            */
+            for(int i = 0; i <= 5; i++)
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write("# ");
+                }
+                Console.WriteLine();
+            }
         }
+        // write a method to print the same pattern as "PrintTriangle()" but upside down(b)
+        static private void PrintTriangleUpsideDown()
+        {
+            for ( int i = 0; i<5; i++)
+            {
+                // +++++++++++++look back over++++++++++++++
+                for ( int j = 0; j<(5-i); j++)
+                {   
+                    Console.Write("# ");
+                }
+                Console.WriteLine();
+            }
+            /* can do it this way either -> jacks way
+             * string hashtag="#";
+             * inti=5;
+             * while(0<=i)
+             * {
+             * int counter=0;
+             * while(counter<i)
+             * {
+             * Console.Write(hashtag);
+             * counter++;
+             * }
+             * Console.Write($"\n");
+             * i--
+             * }
+             * */
+
+        }
+        // write a method to print hashtag pattern an inputted number of times
+        static private void PrintTriangleGeneral()
+        {
+            Console.Write("Enter a number: ");
+            int numberOfTimes=int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= numberOfTimes; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("# ");
+                }
+                Console.WriteLine();
+            }
+        }
+      
 
     }
 }
